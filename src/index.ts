16ee0ebdf9adf29d54e4aaef56b6ffd5df32c191
GuopengLin <17302010022@fudan.edu.cn>
2020-08-10 15:55:56 +0800

    xx

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ export async function main() {
                 }
             }
         }
-        WriteFile("../../tmp", infoStr);
+        WriteFile("test.txt", infoStr);
     });
     extension.Run();
 }
