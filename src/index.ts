0ce9bd603aa25ca9490e6e619c0fdbc39a69a176
GuopengLin <17302010022@fudan.edu.cn>
2020-08-11 15:22:24 +0800

    xx

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ export async function main() {
             // modelGroup.init();
             for (let m of climodel.operationGroups){
                 Info("============== moduleName: "+m["$key"]+" =================");
-                Info(""+yaml(m));
+                Info(""+yaml.dump(m));
                 // let idx1 = 1;
                 // for (let method of m.operations){
                 //     Info("============== method: "+idx1+"  =================");
