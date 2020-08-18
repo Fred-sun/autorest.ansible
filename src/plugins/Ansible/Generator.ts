ec0e5fc7e8d992d6fd182927cb51f574965c2acc
GuopengLin <17302010022@fudan.edu.cn>
2020-08-18 11:28:01 +0800

    xx

--- src/plugins/Ansible/Generator.ts
+++ src/plugins/Ansible/Generator.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+import * as yaml from "node-yaml";
 
 // import { Example } from "../Common/Example";
 
@@ -95,7 +95,28 @@ export async function processRequest(host: Host) {
 
     try {
         const session = await startSession<CodeModel>(host, {}, codeModelSchema);
-        host.WriteFile("model4.yaml",serialize(session.model));
+        // host.WriteFile("model4.yaml",serialize(session.model));
+        let str : string[] = [];
+        let codeModel = session.model;
+        for (let module of codeModel.operationGroups){
+            str.push("============== moduleName: "+module["$key"]+" =================");
+            let idx1 = 1;
+            for (let method of module.operations){
+                str.push("============== method: "+idx1+"  =================");
+                str.push("      method: "+method.requests[0].protocol.http.method);
+                str.push("      name: "+method.language.default.name);
+                str.push("      path:" + method.requests[0].protocol.http.path);
+                str.push("      version:" + method.apiVersions[0].version)
+                idx1++;
+                let idx2 = 1;
+                for (var p of method.parameters){
+                    str.push("============parameter: "+idx2 + "==============")
+                    str.push("" + yaml.dump(p));
+                    idx2++;
+                }
+            }
+        }
+        host.WriteFile("test.txt", str.join(EOL));
         // let model = new AnsibleCodeModel(session);
         // let files: any = await GenerateAll(model, ArtifactType.ArtifactTypeAnsibleRest);
         // for (let f in files) {
